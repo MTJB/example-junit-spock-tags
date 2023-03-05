a41b16115ce2dcdd17f77b6a1b1e5f48b3d90e6e
MTJB <markbrown04@googlemail.com>
2023-03-05 13:58:36 +0000

    Add JUnit & Spock tests w/tags

---
+++ src/main/java/com/mtjb/demo/math/Multiply.java
@@ -0,0 +1,8 @@
+package com.mtjb.demo.math;
+
+public class Multiply {
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/groovy/com.mtjb.demo/MultiplySpockTests.groovy
@@ -0,0 +1,20 @@
+package com.mtjb.demo
+
+import com.mtjb.demo.math.Multiply
+import spock.lang.Specification
+import spock.lang.Tag
+
+@Tag("Spock")
+class MultiplySpockTests extends Specification {
+
+    @Tag("Multiply")
+    void "Test multiple"(int a, int b, int expected) {
+        expect:
+            Multiply.multiply(a, b) == expected
+        where:
+            a  | b  | expected
+            10 | 10 | 100
+            1  | 10 | 10
+            42 | 10 | 420
+    }
+}
