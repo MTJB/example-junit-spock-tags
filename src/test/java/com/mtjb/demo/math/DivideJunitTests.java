66b54a5b72afbc467f95bf3c00b9d65d0d2cab2a
MTJB <markbrown04@googlemail.com>
2023-03-07 16:48:57 +0000

    Rename test - typo

--- src/test/java/com/mtjb/demo/math/DivideJunitTests.java
+++ src/test/java/com/mtjb/demo/math/DivideJunitTests.java
@@ -11,7 +11,7 @@ public class DivideJunitTests {
     @Tag("Divide")
     @ParameterizedTest
     @CsvSource({"10,10,1", "10,1,10", "4200,10,420"})
-    public void testMultiply(int a, int b, int expected) {
+    public void testDivide(int a, int b, int expected) {
         Assertions.assertEquals(expected, Divide.divide(a, b));
     }
 }
